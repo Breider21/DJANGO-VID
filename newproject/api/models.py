99a2e65740d18342567fe03e9f3930495ef4cac3
Breider21 <breiderd-rangels@unilibre.edu.co>
2024-11-08 09:47:32 -0500

    nuevos cambios

--- newproject/api/models.py
+++ newproject/api/models.py
@@ -1,12 +1,8 @@
 from django.db import models
 
-class Contacto(models.Model):
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50)
-    correo = models.EmailField()
-    celular = models.CharField(max_length=15)
-    mensaje = models.TextField()
+class Users(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
 
     def __str__(self):
-        return f"{self.nombre} {self.apellido}"
-
+        return self.name
--- newproject/api/views.py
+++ newproject/api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Users
+from .serializers import UserSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def get_user(request):
+    return Response (UserSerializer({'name': "pedro", "age": 23}).data) # Return a user with name pedro and age 23
+
+@api_view(['POST'])
+def create_user(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
